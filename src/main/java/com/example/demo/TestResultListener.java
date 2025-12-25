a8743a818bc5f6475850c56bc84a2a765b294775
bala-amypo <bala@amypo.in>
2025-12-25 04:52:37 +0000

    User commit

--- src/main/java/com/example/demo/TestResultListener.java
+++ src/main/java/com/example/demo/TestResultListener.java
@@ -1,23 +1,38 @@
 package com.example.demo;
 
-import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.ITestContext;
 
 public class TestResultListener implements ITestListener {
 
     @Override
-    public void onTestSuccess(ITestResult result) {}
+    public void onTestStart(ITestResult result) {
+        System.out.println("Test Started: " + result.getName());
+    }
 
     @Override
-    public void onTestFailure(ITestResult result) {}
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("Test Passed: " + result.getName());
+    }
 
     @Override
-    public void onTestSkipped(ITestResult result) {}
+    public void onTestFailure(ITestResult result) {
+        System.out.println("Test Failed: " + result.getName());
+    }
 
     @Override
-    public void onStart(ITestContext context) {}
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("Test Skipped: " + result.getName());
+    }
 
     @Override
-    public void onFinish(ITestContext context) {}
+    public void onStart(ITestContext context) {
+        System.out.println("Tests Started: " + context.getName());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        System.out.println("Tests Finished: " + context.getName());
+    }
 }
