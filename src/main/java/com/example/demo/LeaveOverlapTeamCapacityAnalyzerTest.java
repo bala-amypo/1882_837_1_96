5a640c3c4a1e4982a23209019878abd6aa206321
bala-amypo <bala@amypo.in>
2025-12-25 09:35:48 +0000

    User commit

---
+++ src/main/java/com/example/demo/LeaveOverlapTeamCapacityAnalyzerTest.java
@@ -0,0 +1,11 @@
+// src/test/java/com/example/demo/LeaveOverlapTeamCapacityAnalyzerTest.java
+package com.example.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+public class LeaveOverlapTeamCapacityAnalyzerTest extends AbstractTestNGSpringContextTests {
+    // Your 70 test cases go here...
+}
