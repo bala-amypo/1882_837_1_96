4536117134303850d15090f671f996b9db9abfbf
bala-amypo <bala@amypo.in>
2025-12-25 06:14:49 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,8 +1,39 @@
 package com.example.demo.security;
 
+import com.example.demo.model.UserAccount;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
-    // Authentication removed
+    private final String jwtSecret = "base64EncodedSecretKeyForDemoPurposeOnly12345678901234567890";
+    private final long jwtExpirationInMs = 3600000;
+
+    public String generateToken(UserAccount user) {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("userId", user.getId())
+                .claim("role", user.getRole())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationInMs))
+                .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(jwtSecret.getBytes()).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public String getEmail(String token) {
+        return Jwts.parserBuilder().setSigningKey(jwtSecret.getBytes()).build().parseClaimsJws(token)
+                .getBody().getSubject();
+    }
 }
